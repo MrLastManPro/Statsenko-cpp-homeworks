7ffa4c3a1a2f4e5b65577906db1cc160f2d504d0
MrLastManPro <47716533+MrLastManPro@users.noreply.github.com>
2025-09-24 18:35:45 +0500

    Update homework1.cpp

--- homework1.cpp
+++ homework1.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+    // Объявление переменной для хранения имени пользователя
+    string userName;
+    
+    // Запрос имени у пользователя
+    cout << "Пожалуйста, введите ваше имя: ";
+    getline(cin, userName);
+    
+    // Вывод персонального приветствия
+    cout << "Привет, " << userName << "! Рад вас видеть!" << endl;
+    
+    return 0;
+}
